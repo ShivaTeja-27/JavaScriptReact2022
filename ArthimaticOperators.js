6ef7e78153ed1631e947fb9ae838e95e6cabdb53
G K S SHIVA TEJA <shivateja.gks@gmail.com>
2023-02-11 18:52:45 +0530

    javascript practice feb

---
+++ ArthimaticOperators.js
@@ -0,0 +1,36 @@
+
+// // <!-- OPERATORS IN JAVASCRIPT (Arthematic, logical, conditional, terinary OPERATORS) -->
+
+// 1--- ArthimaticOperators
+
+console.log("arthematic opertaors in js:::: ")
+
+let a = 10;
+let b = 4;
+
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a / b = ", a/b)
+console.log("a ** b = ", a**b)
+console.log("a * b = ", a*b)
+console.log("a % b = ", a%b)
+
+console.log("++a = ", ++a)
+console.log("a = ", a)
+console.log("a++ = ", a++)
+console.log("a = ", a)
+console.log("--a = ", --a)
+console.log("a = ", a)
+console.log("a-- = ", a--)
+console.log("a = ", a)
+
+
+console.log("a-- = ", a--)
+console.log("a = ", a)
+console.log("--a = ", --a)
+console.log("a = ", a)
+
+
+// next is assignment operators =, +=, -=, **=, *=, /=, %=
+
+
---
+++ AssignmentOperators.js
@@ -0,0 +1,25 @@
+// // <!-- OPERATORS IN JAVASCRIPT (Arthematic, logical, conditional, terinary OPERATORS) -->
+
+// 2--- Assignment Operators
+
+//assignment operators =, +=, -=, **=, *=, /=, %=
+
+let x = 10
+let y = 5
+
+console.log(x+=y) // x = x+y
+console.log(x-=y) // x = x-y
+console.log(x*=y) // x = x*y
+console.log(x/=y) // x = x/y
+console.log(x**=y) // x = x**y
+console.log(x%=y) // x = x%y
+
+// o/p
+
+// 15
+// 10
+// 50
+// 10
+// 100000
+// 0
+
